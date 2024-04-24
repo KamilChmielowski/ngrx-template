002cd6e3c9d206db0f9d6656cb33c8880d3174f0
Kamil Chmielowski <kamil.chmielowski@comarch.com>
2024-04-24 13:20:13 +0200

    feat: register actions

---
+++ src/app/store/app/app.actions.ts
@@ -0,0 +1,11 @@
+import { createAction, props } from '@ngrx/store';
+
+export const setValue1 = createAction(
+  '[App] Set value 1',
+  props<{ value: number }>(),
+);
+
+export const setValue2 = createAction(
+  '[App] Set value 2',
+  props<{ value: string }>(),
+);
--- src/app/store/app/app.reducers.ts
+++ src/app/store/app/app.reducers.ts
@@ -1,7 +1,16 @@
-import { createReducer } from '@ngrx/store';
+import { createReducer, on } from '@ngrx/store';
+
+import { setValue1, setValue2 } from './app.actions';
 
 const initialState = 0;
 const initialState2 = '';
 
-export const value1Reducer = createReducer(initialState);
-export const value2Reducer = createReducer(initialState2);
+export const value1Reducer = createReducer(
+  initialState,
+  on(setValue1, (state, actions) => actions.value),
+);
+
+export const value2Reducer = createReducer(
+  initialState2,
+  on(setValue2, (state, actions) => actions.value),
+);
