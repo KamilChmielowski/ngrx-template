35263445207b9188f4f5ee28185148e804589655
Kamil Chmielowski <kamil.chmielowski@comarch.com>
2024-04-24 13:06:00 +0200

    chore: install store, effects

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,15 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
+import { provideEffects } from '@ngrx/effects';
+import { provideStore } from '@ngrx/store';
+
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    provideStore(),
+    provideEffects(),
+  ]
 };
